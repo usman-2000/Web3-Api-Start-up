0ea8f12a29c33a68f0278eb42fd4cf4dc11ef71a
usmanrahim <usmanrahim2000@gmail.com>
2023-07-03 23:55:52 +0500

    api

--- Api/Controllers/nftController.js
+++ Api/Controllers/nftController.js
@@ -10,3 +10,25 @@ exports.getAllNfts = async (req, res, next) => {
     },
   });
 };
+
+exports.getNft = async (req, res, next) => {
+  const nft = await NFT.findById(req.params.id);
+  res.status(200).json({
+    status: "success",
+    data: {
+      nft,
+    },
+  });
+};
+
+exports.createNft = async (req, res, next) => {
+  console.log(req.body);
+  const newNft = await NFT.create(req.body);
+
+  res.status(201).json({
+    status: "success",
+    data: {
+      nft: newNft,
+    },
+  });
+};
