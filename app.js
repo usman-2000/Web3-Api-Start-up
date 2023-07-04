b2d3007815585a10fa82a89d6256765806ebdf12
usmanrahim <usmanrahim2000@gmail.com>
2023-07-04 08:22:46 +0500

    app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+
+const nftRouter = require("./Api/Routers/nftRouter");
+const userRouter = require("./Api/Routers/userRouter");
+
+// Middleware
+
+const app = express();
+
+app.use(express.json({ limit: "100kb" }));
+app.use(cors());
+app.options("*", cors());
+
+// Routes
+
+app.use("/api/v1/NFTs", nftRouter);
+app.use("/api/v1/user", userRouter);
+
+module.exports = app;
